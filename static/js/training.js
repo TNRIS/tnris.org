bc32458e45b68faff661e51f56393714530b7b51
Marlboro Blend No. 27 <adam.breznicky@twdb.texas.gov>
2020-05-20 11:21:59 -0500

    initial filter dropdown and course filtering js functionality

--- static/js/training.js
+++ static/js/training.js
@@ -1,7 +1,10 @@
 // get training records from tnris api
 // inject training records into html; append html to template html
-function retrieveTraining() {
+function retrieveTraining(queryField, queryValue) {
   var trainingUrl = 'https://api.tnris.org/api/v1/tnris_org/training';
+  if (queryField) {
+    trainingUrl = trainingUrl + "?" + queryField + "=" + queryValue;
+  }
   return fetch(trainingUrl).then(function(response) {
     if (!response.ok) {
       throw new Error('Could not retrieve TNRIS API response for training events.');
@@ -166,11 +169,61 @@ function retrieveTraining() {
 
     anchors.forEach(function(a) {
       a.id ? clicker(a) : '';
-    })
+    });
+  });
+}
+
+// get training records from tnris api to create categories filter of courses
+// inject category filter into html; append html to template html
+// this action is performed in a separate function so that the retrieveTraining()
+// function remains independent and can be re-used when filter applied
+function retrieveTrainingCategories() {
+  var trainingUrl = 'https://api.tnris.org/api/v1/tnris_org/training';
+  return fetch(trainingUrl).then(function(response) {
+    if (!response.ok) {
+      throw new Error('Could not retrieve TNRIS API response for training events.');
+    }
+    return response.json();
+  })
+  .then(function(data) {
+    // set aside categories array for creating unique list from courses
+    var categories = [];
+    // iterate over array of objects in response and do the stuff
+    data.results.forEach(function(t) {
+      // if course category not in array, add it
+      if (!categories.includes(t.category)) {
+        categories.push(t.category);
+      }
+    });
+
+    // loop unique categories array to add options to filter dropdown
+    categories.sort().forEach((c) => {
+      var cat = document.createElement('li');
+      cat.innerHTML = c;
+      cat.setAttribute('class', 'category-dropdown-menu-item');
+      document.getElementById('category-dropdown-menu').appendChild(cat);
+    });
+  })
+  .then(function() {
+    // add event listerner to all category-dropdown-menu-item li tags so onclick
+    // filters displayed courses
+    var menuItems = document.querySelectorAll(".category-dropdown-menu-item");
+
+    menuItems.forEach(function(i) {
+      i.addEventListener("click", function() {
+        // update dropdown to display chosen filter
+        document.getElementById("selected-category-filter").innerHTML = i.innerHTML;
+        // clear all courses which were injected into the training-list
+        document.getElementById("insert-here").innerHTML = "";
+        // re-retrieve the trainings with chosen filter applied to api call as query
+        i.innerHTML === 'All Courses' ? retrieveTraining() : retrieveTraining('category', i.innerHTML);
+      });
+    });
   })
 }
 
 // run main retrieveTraining function when on education page
 if (location.pathname.includes('/education')) {
   retrieveTraining();
+  retrieveTrainingCategories();
 }
