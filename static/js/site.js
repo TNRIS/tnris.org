8779093a72a6f42aecf5cabe0b8e4f8e2c8aaefc
Logan <logan.smithson@twdb.texas.gov>
2021-03-03 13:43:51 -0600

    Issue #259

--- static/js/site.js
+++ static/js/site.js
@@ -200,3 +200,17 @@ var Clipboard = require('clipboard/dist/clipboard.min');
   })
 
 })(jQuery);
+
+
+//Navbar dropdown on hover]
+
+$( ".dropdown-menu" ).css('margin-top',0);
+$( ".dropdown" )
+  .mouseover(function() {
+    $( this ).addClass('show').attr('aria-expanded',"true");
+    $( this ).find('.dropdown-menu').addClass('show');
+  })
+  .mouseout(function() {
+    $( this ).removeClass('show').attr('aria-expanded',"false");
+    $( this ).find('.dropdown-menu').removeClass('show');
+  });
