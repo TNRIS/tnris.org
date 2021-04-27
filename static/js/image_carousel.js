e7034d186c085d0157d166ff239c23c83e62614a
Logan <logan.smithson@twdb.texas.gov>
2021-04-27 15:07:11 -0500

    cleaning up js file

--- static/js/image_carousel.js
+++ static/js/image_carousel.js
@@ -1,10 +1,8 @@
 // Home page carousel
 // get carousel images from tnris api
-function retrieveCarousel(queryField, queryValue) {
+function retrieveCarousel() {
     var carouselUrl = 'https://api.tnris.org/api/v1/tnris_org/carousel_image';
-    if (queryField) {
-        carouselUrl = carouselUrl + "?" + queryField + "=" + queryValue;
-    }
+
     return fetch(carouselUrl).then(function (response) {
         if (!response.ok) {
             throw new Error('Could not retrieve TNRIS API response for carousel images.');
@@ -12,29 +10,22 @@ function retrieveCarousel(queryField, queryValue) {
         return response.json();
     })
         .then(function (data) {
-            data.results = data.results.sort(function (a, b) {
-                return b.last_modified.localeCompare(a.last_modified)
+            var count = 0;
 
-            });
             data.results.forEach(function (t) {
                 // use api values to create clean variables to use in html below
+                var strCount = count.toString();
                 $(document).ready(function () {
-                    for (let j = 0; j < 1; j++) {
-                        $('<div class="carousel-item"><img class="d-block img-fluid" src="' + t.image_url + '" alt="' + t.image_name + '"> <div class="carousel-caption"><p>' + t.carousel_caption + '</p> </div>  </div>').appendTo('.carousel-inner');
 
-                    }
+                    $('<div class="carousel-item"><img class="d-block img-fluid" src="' + t.image_url + '" alt="' + t.image_name + '"> <div class="carousel-caption"><p>' + t.carousel_caption + '</p> </div>  </div>').appendTo('.carousel-inner');
+                    $('<li data-target="#tnris-carousel" data-slide-to="' + strCount + '"></li>').appendTo('.carousel-indicators')
+
                     $('.carousel-item').first().addClass('active');
+                    $('.carousel-indicators > li').first().addClass('active');
                 });
+                count += 1;
             });
 
-            data.results
-            $(document).ready(function () {
-                for (let i = 0; i < data.count; i++) {
-                    $('<li data-target="#tnris-carousel" data-slide-to="' + i + '"></li>').appendTo('.carousel-indicators')
-
-                }
-                $('.carousel-indicators > li').first().addClass('active');
-            });
         })
 }
 
