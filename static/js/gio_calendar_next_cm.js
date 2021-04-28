11984173f18788d3e1b5969d83e487af3ab47f82
Logan <logan.smithson@twdb.texas.gov>
2021-04-28 14:06:41 -0500

    minor edits
    -added closing <p> tags to 2018 forum page
    -changes to icons and formatting of community meeting event box

--- static/js/gio_calendar_next_cm.js
+++ static/js/gio_calendar_next_cm.js
@@ -17,7 +17,7 @@ function retrieveNextCommunityMeeting() {
       // create empty dom div element then populate with api response information
       var event = document.createElement('div');
       // check if various properties exist, and set variables if so
-      var timeRange = e.pretty_time ? `<time><i class="fa fa-time"></i> ${e.pretty_time}</time><br>` : '';
+      var timeRange = e.pretty_time ? `<time>${e.pretty_time}</time><br>` : '';
       var shortDescription = e.short_description ? `<p>${e.short_description}</p>` : '';
       var generalLocation = e.location ? `<location><strong>Location:</strong><br>${e.location}</location>` : '';
       var eventLink = e.event_url ? `<hr><button class="event-link">
@@ -30,12 +30,13 @@ function retrieveNextCommunityMeeting() {
       event.innerHTML =
         `
           <h3>
-            <strong>${e.title}</strong>
+            <strong>${e.title}</strong> <i class="fa fa-star" style="color: #1e8dc1;"></i>
           </h3>
           <h5>
-            ${e.pretty_date}<i class="fa fa-star" style="color: #1e8dc1;"></i>
+            ${e.pretty_date} 
           </h5>
           ${timeRange}
+          <br>
           ${shortDescription}
           ${generalLocation}
           <div class="justify-content-between">
