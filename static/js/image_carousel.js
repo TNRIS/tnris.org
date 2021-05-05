aecafe7ce80229846d75be3e8988461e5ab296df
Logan <logan.smithson@twdb.texas.gov>
2021-05-05 15:02:39 -0500

    added hyperlink to carousel

--- static/js/image_carousel.js
+++ static/js/image_carousel.js
@@ -12,30 +12,31 @@ function retrieveCarousel() {
         .then(function (data) {
             var count = 0;
 
-            //test for duplicates in carousel_order field (carousel_caption used as placeholder)
-            const detectDuplicates = (arr, selector) => { 
-                const table = new Set();
-                for (const element of arr) { 
-                    const discriminator = selector(element);
-                    if (table.has(discriminator)) return true; 
-                    table.add(discriminator); 
-                } 
-                return false; 
-            }; 
-            const duplicatesExist = detectDuplicates(data.results, x => x.carousel_caption);
+            // // test for duplicates in carousel_order field (not necessary with ordering handled through API)
+            // const detectDuplicates = (arr, selector) => { 
+            //     const table = new Set();
+            //     for (const element of arr) { 
+            //         const discriminator = selector(element);
+            //         if (table.has(discriminator)) return true; 
+            //         table.add(discriminator); 
+            //     } 
+            //     return false; 
+            // }; 
+            // const duplicatesExist = detectDuplicates(data.results, x => x.carousel_order);
 
-            //if matching sort orders exist, sort by last_modified, otherwise sort by carousel_order (carousel_caption used as placeholder)
-            if (duplicatesExist) data.results.sort((a, b) => 
-            b.last_modified.localeCompare(a.last_modified));
-            else data.results.sort((a, b) => a.image_name.localeCompare(b.image_name));
+            // // if matching sort orders exist, sort by last_modified, otherwise sort by carousel_order
+            // if (duplicatesExist) data.results.sort((a, b) => 
+            // b.last_modified.localeCompare(a.last_modified));
+            // else data.results.sort((a, b) => a.carousel_order.localeCompare(b.carousel_order));
 
             data.results.forEach(function (t) {
                 // use api values to create clean variables to use in html below
                 var strCount = count.toString();
+                var hyperlink = t.carousel_link ? `<a href="${t.carousel_link}" target="_blank">` : '';
                 $(document).ready(function () {
 
-                    $('<div class="carousel-item"> <a href="' + t.image_url + '" target="_blank"> <img class="d-block img-fluid" src="' + t.image_url + '" alt="' + t.image_name + '"> </a> <div class="carousel-caption"><p>' + t.carousel_caption + '</p> </div>  </div>').appendTo('.carousel-inner');
-                    $('<li data-target="#tnris-carousel" data-slide-to="' + strCount + '"></li>').appendTo('.carousel-indicators')
+                    $('<div class="carousel-item">' + hyperlink + '<img class="d-block img-fluid" src="' + t.image_url + '" alt="' + t.image_name + '"> </a> <div class="carousel-caption"><p>' + t.carousel_caption + '</p> </div>  </div>').appendTo('.carousel-inner');
+                    $('<li data-target="#tnris-carousel" data-slide-to="' + strCount + '"></li>').appendTo('.carousel-indicators');
 
                     $('.carousel-item').first().addClass('active');
                     $('.carousel-indicators > li').first().addClass('active');
